4c5c927e80ec60540b066e157561f7373c52d348
Selma Waller <selmawaller@gmail.com>
2020-03-15 08:57:03 +0100

    image styling

--- src/components/characters.js
+++ src/components/characters.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 const Characters = props => {
   const {id, name, avatar, status, species, gender, episode} = props;
   return (
-    <div className="innerCard textLeft linkedCard boxShadow">
+    <div className="innerCard textLeft linkedCard boxShadow character">
       <Link to={`/character-specific/${id}`}>
         <h3>{name}</h3>
         <img src={avatar} alt={name} />
